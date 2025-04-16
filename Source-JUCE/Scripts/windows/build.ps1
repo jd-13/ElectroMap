8a9a23f21931b5f5105957cf378e2b391edd3672
jd-13 <jackd8989@gmail.com>
2025-04-16 14:19:02 +0100

    Try forward slash in global path

--- Source-JUCE/Scripts/windows/build.ps1
+++ Source-JUCE/Scripts/windows/build.ps1
@@ -13,7 +13,7 @@ Expand-Archive -LiteralPath $JUCE_ZIP -DestinationPath (Join-Path -Path $SCRIPT_
 Write-Output "=== Generating project ==="
 $PROJECT_DIR = Join-Path -Path $SCRIPT_DIR -ChildPath "..\..\Projects"
 $JUCE_HOME = Join-Path -Path $SCRIPT_DIR -ChildPath "..\..\JUCE"
-& "$JUCE_HOME\Projucer.exe" --set-global-search-path windows defaultJuceModulePath "$JUCE_HOME\modules"
+& "$JUCE_HOME\Projucer.exe" --set-global-search-path windows defaultJuceModulePath "../../JUCE/modules"
 & "$JUCE_HOME\Projucer.exe" --resave "$PROJECT_DIR\ElectroMap\ElectroMap.jucer"
 
 Write-Output "=== Starting ElectroMap build ==="
