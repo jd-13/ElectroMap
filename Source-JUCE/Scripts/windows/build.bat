1bc2c9e019ab6fb15686160c5a057f77882cfda4
jd-13 <jackd8989@gmail.com>
2025-04-16 15:06:23 +0100

    Try bat file for windows build

---
+++ Source-JUCE/Scripts/windows/build.bat
@@ -0,0 +1,23 @@
+set SCRIPT_DIR=%~dp0
+
+ECHO "=== Downloading JUCE ==="
+set JUCE_VERSION=8.0.7
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest https://github.com/juce-framework/JUCE/releases/download/%JUCE_VERSION%/juce-%JUCE_VERSION%-windows.zip -OutFile %SCRIPT_DIR%..\..\juce-%JUCE_VERSION%-windows.zip}"
+powershell -Command "& {Expand-Archive -LiteralPath %SCRIPT_DIR%..\..\juce-%JUCE_VERSION%-windows.zip -DestinationPath %SCRIPT_DIR%..\..}"
+
+ECHO "=== Generating project ==="
+set JUCE_HOME=%SCRIPT_DIR%..\..\JUCE
+set PROJECT_DIR=%SCRIPT_DIR%..\..\Projects
+%JUCE_HOME%\Projucer --set-global-search-path windows defaultJuceModulePath %JUCE_HOME%\modules
+%JUCE_HOME%\Projucer --resave %PROJECT_DIR%\ElectroMap\ElectroMap.jucer
+
+Write-Output "=== Starting ElectroMap build ==="
+msbuild -version
+cd %PROJECT_DIR%\ElectroMap\Builds\VisualStudio2019
+msbuild ElectroMap.sln /p:Configuration=Release /p:Platform=x64 || exit /b
+
+ECHO "=== Collecting artefacts ==="
+cd %SCRIPT_DIR%
+mkdir -p dist
+cd dist
+copy %PROJECT_DIR%\ElectroMap\Builds\VisualStudio2019\x64\Release\ElectroMap\ElectroMap.exe || exit /b
